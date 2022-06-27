0b4e7edd9012d6c972c42371087dbc4ff6f1091b
Roberto Ferreira <78699824+otrebor7@users.noreply.github.com>
2022-06-27 17:16:17 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -10,12 +10,12 @@
 
 #Pesquisa e utilização de novas técnicas e estruturas do Python.
 
-#Pacotes necessários
+#Pacotes necessários #Necessário instalar as dependências pelo pip: pip install -r requirements.txt
 import qrcode
 from PIL import ImageShow
 import os.path 
 
-alunos_cadastrados = []
+alunos_cadastrados = [] #Lista vazia que irá receber os dados
 
 def cadastro(dados_aluno): # Função para cadastro de usuário
     nome = input('Digite o nome do aluno(a): ')
@@ -23,26 +23,26 @@ def cadastro(dados_aluno): # Função para cadastro de usuário
     curso = input('Nome do curso: ')
     periodo = int(input('Nº do perído: '))
 
-    juntar = nome + " " + str(matricula) + " " +  curso + " " + str(periodo)
-    alunos_cadastrados.append(juntar)
+    juntar = nome + " " + str(matricula) + " " +  curso + " " + str(periodo) #Variável que recebe os todos os dados
+    alunos_cadastrados.append(juntar) #A lista recebe a variável "juntar"
 
-    dados_aluno = qrcode.make(alunos_cadastrados) #Arquivo png(qrcode) é gerado com o numero da matrícula.
-    dados_aluno.save("{}.png".format(matricula))
+    dados_aluno = qrcode.make(alunos_cadastrados) #Arquivo png(qrcode) é gerado com o numero da matrícula como nome e com a extensão .png
+    dados_aluno.save("{}.png".format(matricula)) #Salvando o Qrcode
 
-    with open('lista.txt', 'a+') as arquivo: #Criação de arquivo TXT para guardar as informaçãoes dos(as) estudantes.
-        for aluno in alunos_cadastrados:
-            arquivo.write('\n' + aluno)
+    with open('lista.txt', 'a+') as arquivo: #Criação de arquivo txt para guardar as informaçãoes dos(as) estudantes.
+        for aluno in alunos_cadastrados: #Percorrendo a lista
+            arquivo.write('\n' + aluno) #Escrevendo no arquivo .txt os elementos da lista. 
 
-    print("QrCode criado e cadastro realizado com sucesso.")
+    print("QrCode criado e cadastro realizado com sucesso.") #Mostrando ao usuário que o QRcode foi criado.
     ImageShow.show(dados_aluno) #Abre o aplicativo de galeria de fotos e mostra o QRcode na tela.
     print(alunos_cadastrados)
 
 def identificador(matricula): #Função que identificas, na pasta do codigo, se há ou matricula cadastrada. 
     while True: #Verifica se a matricula possui QRcode.
-        if (os.path.isfile("{}.png".format(matricula))):
-            print("Essa matrícula já possui QrCode.")
-        else: #Senão exitir = qrcode gerado, perguta se quer criar um novo.
-            resposta = input("Essa matrícula ainda não possui QrCode. Deseja realizar o cadastro? [S/N] ").upper()
+        if (os.path.isfile("{}.png".format(matricula))): #Percorre a pasta que está o programa, e verifica se há um qrcode com o nome da matrícula digitada
+            print("Essa matrícula já possui QrCode.") #Mostra a mensagem caso na busca anterior o qrcode tenha sido encontradado
+        else: #Caso não haja, perguta se quer criar um novo.
+            resposta = input("Essa matrícula ainda não possui QrCode. Deseja realizar o cadastro? [S/N] ").upper() #Input com a resposta se quer ou não efetuar o cadastro de uma nova matrícula e geração de qrcode.
             while resposta != "S" and resposta != "N":
                 resposta = input(
                     "Digite uma resposta válida: [S] para fazer um novo cadastro ou [N] para sair\nEssa matrícula ainda não possui QrCode. Deseja realizar o cadastro? [S/N] ").upper()
@@ -54,9 +54,9 @@ def identificador(matricula): #Função que identificas, na pasta do codigo, se 
 
 
 while True: #Após digitar a matricula, a função identificador é chamada, para veficiar a exitestência da matricula. 
-    matricula = int(input("Digite o n° de matrícula: "))
-    identificador(matricula)
-    resposta = input("Deseja realizar nova busca por matrícula? [S/N] ").upper()
+    matricula = int(input("Digite o n° de matrícula: ")) #Primeiro input do programa
+    identificador(matricula) #Chama a função identificador(matricula) para ver confirmar se há ou não uma matricula realizada.
+    resposta = input("Deseja realizar nova busca por matrícula? [S/N] ").upper() 
     while resposta != "N" and resposta != "S":
         resposta = input(
             "Digite uma resposta válida: [S] para fazer uma nova busca ou [N] para finalizar o programa\nDeseja realizar nova busca por matrícula? [S/N] ").upper()
